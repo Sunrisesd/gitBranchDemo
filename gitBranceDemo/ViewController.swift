373c386c32d8f7b20b7a0d91f57a4520b234a832
weirisheng <weirisheng@tax.com>
2019-07-18 16:14:17 +0800

    Initial Commit

---
+++ gitBranceDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitBranceDemo
+//
+//  Created by Sunrise on 2019/7/18.
+//  Copyright © 2019 ANSO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
